9823802477c858951888ea9c2416e79a6382cd82
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-17 13:36:19 +0200

    Initializes a detail view

---
+++ Food2Fork/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Food2Fork
+//
+//  Created by Utheim Sverdrup, Ulrik on 17.08.2017.
+//  Copyright © 2017 Utheim Sverdrup, Ulrik. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+    
+    
+    
+}
