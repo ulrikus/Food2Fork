9cd60847a25c11cd7aee5781e5ece141e9914aa8
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-17 15:59:14 +0200

    Adds dummy table in detail table view

--- Food2Fork/DetailViewController.swift
+++ Food2Fork/DetailViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class DetailViewController: UITableViewController {
     
-    var recipe: Recipe?
+    var recipe: ListRecipe?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = recipe?.title
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.foodWhite]
+        
+        self.tableView.backgroundColor = .foodBlack
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.StringLiterals.ReuseCellIdentifier)!
+        
+        cell.textLabel?.text = "Test \(indexPath.row+1)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let recipeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 375, height: 50))
+        let imageURL = URL(string: (recipe?.imageUrlString)!)
+        
+        performTaskInBackground() {
+            if let imageData = try? Data(contentsOf: imageURL!) {
+                performUIUpdatesOnMain {
+                    recipeImageView.image = UIImage(data: imageData)
+                }
+            }
+        }
+        return UIView()
+    }
 }
+
