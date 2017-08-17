796908234e21c72129ea8c88b373228153a13628
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-17 13:23:46 +0200

    Displays request result in custom table view

--- Food2Fork/TableViewController.swift
+++ Food2Fork/TableViewController.swift
@@ -11,29 +11,44 @@ import UIKit
 class TableViewController: UITableViewController, UISearchBarDelegate {
 
     var searchController: UISearchController!
-    var resultsController = UITableViewController()
     var searchPhrase = String()
-    var recipes = [String]()
+    var food2ForkImagesResult = [RecipeImage]()    // For storing image titles and URLs
+    let foodGreen = UIColor(red: 130/255, green: 212/255, blue: 90/255, alpha: 1)
+    let foodBlack = UIColor(red: 49/255, green: 49/255, blue: 49/255, alpha: 1)
+    
+    struct RecipeImage {
+        let title: String
+        let urlString: String
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.resultsController.tableView.dataSource = self
-        self.resultsController.tableView.delegate = self
-        
-        self.searchController = UISearchController(searchResultsController: self.resultsController)
+        self.searchController = UISearchController(searchResultsController: nil)
         self.navigationItem.titleView = self.searchController.searchBar
-
-        navigationController?.navigationBar.barTintColor = UIColor.black
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        
+        navigationController?.navigationBar.barTintColor = foodBlack
+        navigationController?.navigationBar.tintColor = foodGreen
         
         self.tableView.registerNib(CustomRecipeCell.self)
-        self.resultsController.tableView.register(CustomRecipeCell.self)
+        self.tableView.backgroundColor = foodBlack
         definesPresentationContext = true
 
         self.searchController.searchBar.delegate = self
     }
     
     // MARK: Search Bar
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let methodParameters: [String: AnyObject] = [
+            Constants.Food2ForkParameterKeys.SearchQuery: searchController.searchBar.text as AnyObject,
+            Constants.Food2ForkParameterKeys.APIKey: Constants.Food2ForkParameterValues.APIKey as AnyObject
+        ]
+        displayImageFromFood2ForkBySearch(methodParameters)
+        
+        print("Search phrase: \(self.searchController.searchBar.text!)")
+    }
     
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         searchBar.text = searchPhrase   // Keep text from former search
@@ -45,24 +60,124 @@ class TableViewController: UITableViewController, UISearchBarDelegate {
     // MARK: Table View
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return food2ForkImagesResult.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeue(CustomRecipeCell.self, for: indexPath)
         
-        cell.recipeTitleLabel.text = "Test \(indexPath.row+1)"
+        let recipeImage = food2ForkImagesResult[indexPath.row]
+        let imageURL = URL(string: recipeImage.urlString)
         
-        if indexPath.row % 2 == 0 {
-            cell.recipeImage.image = #imageLiteral(resourceName: "testImageRecipe")
-        } else {
-            cell.recipeImage.image = #imageLiteral(resourceName: "testImageRecipe3")
+        cell.recipeImage?.image = UIImage()
+        cell.recipeTitleLabel?.text = recipeImage.title
+        
+        performTaskInBackground() {
+            if let imageData = try? Data(contentsOf: imageURL!) {
+                performUIUpdatesOnMain {
+                    cell.recipeImage?.image = UIImage(data: imageData)
+                }
+            }
         }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+    }
+    
+    fileprivate func displayImageFromFood2ForkBySearch(_ methodParameters: [String: AnyObject]) {
+        
+        // Create session and request
+        let session = URLSession.shared
+        let request = URLRequest(url: food2ForkSearchURLFromParameters(methodParameters))
+        
+        // Create network request
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            func displayError(_ error: String) {
+                print(error)
+            }
+            
+            // GUARD: Was there an error?
+            guard (error == nil) else {
+                displayError("There was an error with your request: \(String(describing: error))")
+                return
+            }
+            
+            // GUARD: Did we get a successful 2XX response?
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                displayError("Your request returned a status code other than 2XX.")
+                return
+            }
+            
+            // GUARD: Was there any data returned?
+            guard let data = data else {
+                displayError("No data was returned by the request.")
+                return
+            }
+            
+            let parsedResult: [String: AnyObject]
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String: AnyObject]
+            } catch {
+                displayError("Could not parse the data as JSON.")
+                return
+            }
+            
+            // GUARD: Is the "recipes" key in out result?
+            guard let recipesArray = parsedResult[Constants.Food2ForkResponseKeys.Recipes] as? [[String: AnyObject]] else {
+                displayError("Cannot find keys '\(Constants.Food2ForkResponseKeys.Recipes)' in \(parsedResult).")
+                return
+            }
+            
+            if recipesArray.count == 0 {
+                displayError("No recipes found. Search again.")
+                return
+            } else {
+                var tempArray = [RecipeImage]()
+                for recipe in recipesArray {
+                    // GUARD: Does our image have a key for 'image_url'?
+                    guard let imageURLString = recipe[Constants.Food2ForkResponseKeys.ImageUrl] as? String else {
+                        displayError("Cannot find key '\(Constants.Food2ForkResponseKeys.ImageUrl)'")
+                        continue
+                    }
+                    
+                    if let recipeTitle = recipe[Constants.Food2ForkResponseKeys.Title] as? String, recipeTitle != "" {
+                        let image = RecipeImage(title: recipeTitle, urlString: imageURLString)
+                        tempArray.append(image)
+                    } else {
+                        let image = RecipeImage(title: "(Untitled)", urlString: imageURLString)
+                        tempArray.append(image)
+                    }
+                }
+                performUIUpdatesOnMain {
+                    self.food2ForkImagesResult = tempArray
+                    self.tableView.reloadData()
+//                    self.resultsController.tableView.reloadData()
+                    print("Number of images: \(self.food2ForkImagesResult.count)")
+                }
+            }
+        }
+        
+        // Start the task
+        task.resume()
+    }
+
+    
+    // MARK: Helper for Creating a URL from Parameters
+    private func food2ForkSearchURLFromParameters(_ parameters: [String: AnyObject]) -> URL {
+        var components = URLComponents()
+        components.scheme = Constants.Food2Fork.APIScheme
+        components.host = Constants.Food2Fork.APIHost
+        components.path = Constants.Food2Fork.APIPathSearch
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        return components.url!
     }
 }
 
