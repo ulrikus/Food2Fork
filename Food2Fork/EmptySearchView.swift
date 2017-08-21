252c773b2cb14da449cf5553f51acffe6908b3a6
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-21 13:04:34 +0200

    Adds emptySearchView class and changes the layout in the xib

---
+++ Food2Fork/EmptySearchView.swift
@@ -0,0 +1,13 @@
+//
+//  EmptySearchView.swift
+//  Food2Fork
+//
+//  Created by Utheim Sverdrup, Ulrik on 21.08.2017.
+//  Copyright © 2017 Utheim Sverdrup, Ulrik. All rights reserved.
+//
+
+import UIKit
+
+class EmptySearchView: UIView {
+    
+}
