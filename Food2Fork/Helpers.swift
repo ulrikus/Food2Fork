95f3fe7770c5ad4db8e0a02ea3c65ed9cd5cfc16
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-24 10:26:49 +0200

    Adds helper methods for UICollectionView

--- Food2Fork/Helpers.swift
+++ Food2Fork/Helpers.swift
@@ -27,6 +27,23 @@ public extension UITableView {
     }
 }
 
+extension UICollectionViewCell: Identifiable {}
+
+public extension UICollectionView {
+    
+    public func register(_ cellClass: UICollectionViewCell.Type) {
+        self.register(cellClass.self, forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+    }
+    
+    public func registerNib(_ cellClass: UICollectionViewCell.Type) {
+        self.register(UINib.init(nibName: String(describing: cellClass), bundle: nil), forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+    }
+    
+    public func dequeue<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T where T: Identifiable, T: UICollectionViewCell {
+        return self.dequeueReusableCell(withReuseIdentifier: cellClass.reuseIdentifier, for: indexPath) as! T
+    }
+}
+
 extension UIView {
     /// Helper method to instantiate a nib file.
     ///
