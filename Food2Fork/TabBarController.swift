b27ae1ab56b45eacb36e6fb73cf1a55acd59fa7b
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-25 09:35:04 +0200

    Removes tab bar item text

--- Food2Fork/TabBarController.swift
+++ Food2Fork/TabBarController.swift
@@ -16,6 +16,11 @@ class TabBarController: UITabBarController {
         tabBar.barTintColor = .foodBlack
         tabBar.backgroundColor = .foodBlack
         tabBar.tintColor = .foodGreen
+        
+        // Removes tab bar item text
+        tabBar.items?.forEach({ item in
+            item.title = ""
+            item.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        })
     }
-    
 }
