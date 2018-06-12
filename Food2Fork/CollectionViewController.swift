de0c5520a3038c16c66b7394f10ddb143f0da759
Ulrik Utheim Sverdrup <ulrik.utheim.sverdrup@gmail.com>
2018-06-12 13:44:20 +0200

    Rearranges the collectionViewController and removes the flow layout delegate

--- Food2Fork/CollectionViewController.swift
+++ Food2Fork/CollectionViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class CollectionViewController: UICollectionViewController, UISearchBarDelegate {
+class CollectionViewController: UICollectionViewController {
     
     var searchController: UISearchController!
     var recipeToPass: ListRecipe?
+    var flowLayout = ColumnFlowLayout()
     
     private var emptySearchView: EmptySearchView {
         return self.view as! EmptySearchView
@@ -19,99 +20,36 @@ class CollectionViewController: UICollectionViewController, UISearchBarDelegate 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let emptySearchView = UIView.instanceFromNib() as EmptySearchView
-        self.collectionView?.backgroundView = emptySearchView
-        
-        self.searchController = UISearchController(searchResultsController: nil)
-        self.navigationItem.titleView = self.searchController.searchBar
-        self.searchController.hidesNavigationBarDuringPresentation = false
-        self.searchController.searchBar.keyboardAppearance = .alert
-        self.searchController.dimsBackgroundDuringPresentation = false
-        
+        collectionView?.backgroundView = emptySearchView
+        collectionView?.registerNib(CustomRecipeCell.self)
+        collectionView?.backgroundColor = .foodBlack
+        collectionView?.collectionViewLayout = flowLayout
+        collectionView?.delegate = self
+
+        searchController = UISearchController(searchResultsController: nil)
+        navigationItem.titleView = searchController.searchBar
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.keyboardAppearance = .alert
+        searchController.dimsBackgroundDuringPresentation = false
+
         navigationController?.navigationBar.barTintColor = .foodBlack
         navigationController?.navigationBar.tintColor = .foodGreen
         
-        self.collectionView?.registerNib(CustomCollectionViewRecipeCell.self)
-                
-        self.collectionView?.backgroundColor = .foodBlack
         definesPresentationContext = true
-        
-        self.searchController.searchBar.delegate = self
+
+        searchController.searchBar.delegate = self
     }
-    
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         collectionView?.reloadData()
     }
     
-    // MARK: Search Bar
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        NSObject.cancelPreviousPerformRequests(withTarget: self)
-        
-        guard let searchBarText = searchController.searchBar.text else {
-            return
-        }
-        
-        if searchBarText.isEmpty {
-            RecipesProvider.sharedProvider.listRecipes = []
-            collectionView?.backgroundView?.isHidden = false
-            collectionView?.reloadData()
-        } else {
-            perform(#selector(getRecipeFromFood2ForkBySearch), with: nil, afterDelay: 1.0)
-        }
-    }
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        searchBar.text = RecipesProvider.sharedProvider.lastSearchString   // Keep text from former search
-    }
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        RecipesProvider.sharedProvider.lastSearchString = searchBar.text!  // Store searchPhrase
-    }
-    
-    // MARK: Collection View
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if RecipesProvider.sharedProvider.listRecipes.count == 0 {
-            self.collectionView?.backgroundView?.isHidden = false
-        } else {
-            self.collectionView?.backgroundView?.isHidden = true
-        }
-        
-        return RecipesProvider.sharedProvider.listRecipes.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeue(CustomCollectionViewRecipeCell.self, for: indexPath)
-        
-        let recipeImage = RecipesProvider.sharedProvider.listRecipes[indexPath.row]
-        let imageURL = URL(string: recipeImage.imageUrlString)
-        
-        cell.recipeImage?.image = UIImage()
-        cell.recipeTitleLabel?.text = recipeImage.title
-        
-        performTaskInBackground() {
-            if let imageData = try? Data(contentsOf: imageURL!) {
-                performUIUpdatesOnMain {
-                    cell.recipeImage?.image = UIImage(data: imageData)
-                }
-            }
-        }
-        
-        return cell
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-        
-        let recipe = RecipesProvider.sharedProvider.listRecipes[indexPath.row]
-        recipeToPass = recipe
-        
-        performSegue(withIdentifier: Constants.StringLiterals.SegueIdentifier, sender: self)
-    }
-    
     // MARK: Segue
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == Constants.StringLiterals.SegueIdentifier)
         {
@@ -121,6 +59,7 @@ class CollectionViewController: UICollectionViewController, UISearchBarDelegate 
     }
     
     // MARK: Network request
+    
     @objc fileprivate func getRecipeFromFood2ForkBySearch() {
         guard let searchBarText = searchController.searchBar.text else {
             return
@@ -154,29 +93,76 @@ class CollectionViewController: UICollectionViewController, UISearchBarDelegate 
         alertController.addAction(okAction)
         
         // Present the controller
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
 }
 
-extension CollectionViewController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let numberOfItemsPerRow = CGFloat(3)
-        let screenSize = UIScreen.main.bounds
-        let itemSize = CGSize(width: (screenSize.width/numberOfItemsPerRow - numberOfItemsPerRow*2), height: 150)
+// MARK: - CollectionView delegate and data source
+
+extension CollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if RecipesProvider.sharedProvider.listRecipes.count == 0 {
+            self.collectionView?.backgroundView?.isHidden = false
+        } else {
+            self.collectionView?.backgroundView?.isHidden = true
+        }
         
-        return itemSize
+        return RecipesProvider.sharedProvider.listRecipes.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        let margin = CGFloat(4)
-        return UIEdgeInsets(top: 0, left: margin, bottom: 0, right: margin)
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeue(CustomRecipeCell.self, for: indexPath)
+        
+        let recipeImage = RecipesProvider.sharedProvider.listRecipes[indexPath.row]
+        let imageURL = URL(string: recipeImage.imageUrlString)
+        
+        cell.recipeImage?.image = UIImage()
+        cell.recipeTitleLabel?.text = recipeImage.title
+        
+        performTaskInBackground() {
+            if let imageData = try? Data(contentsOf: imageURL!) {
+                performUIUpdatesOnMain {
+                    cell.recipeImage?.image = UIImage(data: imageData)
+                }
+            }
+        }
+        
+        return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 4
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+        let recipe = RecipesProvider.sharedProvider.listRecipes[indexPath.row]
+        recipeToPass = recipe
+        
+        performSegue(withIdentifier: Constants.StringLiterals.SegueIdentifier, sender: self)
     }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 4
+}
+
+// MARK: UISearchBarDelegate
+
+extension CollectionViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        NSObject.cancelPreviousPerformRequests(withTarget: self)
+        
+        guard let searchBarText = searchController.searchBar.text else {
+            return
+        }
+        
+        if searchBarText.isEmpty {
+            RecipesProvider.sharedProvider.listRecipes = []
+            collectionView?.backgroundView?.isHidden = false
+            collectionView?.reloadData()
+        } else {
+            perform(#selector(getRecipeFromFood2ForkBySearch), with: nil, afterDelay: 1.0)
+        }
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.text = RecipesProvider.sharedProvider.lastSearchString   // Keep text from former search
+    }
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        RecipesProvider.sharedProvider.lastSearchString = searchBar.text!  // Store searchPhrase
     }
 }
