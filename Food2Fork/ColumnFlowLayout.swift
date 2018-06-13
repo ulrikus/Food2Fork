3614d55eb912a56c3c6e917a89161108cc09ccc2
Ulrik Utheim Sverdrup <ulrik.utheim.sverdrup@gmail.com>
2018-06-13 11:01:20 +0200

    Adds complexity to look better on iPad

--- Food2Fork/ColumnFlowLayout.swift
+++ Food2Fork/ColumnFlowLayout.swift
@@ -13,18 +13,32 @@ class ColumnFlowLayout: UICollectionViewFlowLayout {
     override func prepare() {
         super.prepare()
         
+        minimumInteritemSpacing = 4
+        
         guard let collectionView = collectionView else { return }
-        let horizontalLayoutMargin = (collectionView.layoutMargins.right + collectionView.layoutMargins.left) / 2
+        let horizontalLayoutMargin = (collectionView.layoutMargins.right + collectionView.layoutMargins.left) / 2 + minimumInteritemSpacing
         
         let availableWidth = collectionView.bounds.insetBy(dx: horizontalLayoutMargin, dy: collectionView.layoutMargins.top).width
         
-        let minColumnWidth: CGFloat = 300
-        let maxNumberOfColumns = Int(availableWidth / minColumnWidth)
+        var minColumnWidth: CGFloat {
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                return 340
+            } else {
+                return 300
+            }
+        }
+        let maxNumberOfColumns = Int((availableWidth + horizontalLayoutMargin) / minColumnWidth)
         let cellWidth = (availableWidth / CGFloat(maxNumberOfColumns)).rounded(.down)
         
-        itemSize = CGSize(width: cellWidth, height: 150)
-        
-        sectionInset = UIEdgeInsets(top: minimumInteritemSpacing, left: 0, bottom: 0, right: 0)
+        var cellHeight: CGFloat {
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                return 200
+            } else {
+                return 150
+            }
+        }
+        itemSize = CGSize(width: cellWidth, height: cellHeight)
+        sectionInset = UIEdgeInsets(top: minimumInteritemSpacing, left: minimumInteritemSpacing, bottom: 0, right: minimumInteritemSpacing)
         sectionInsetReference = .fromSafeArea
     }
 }
