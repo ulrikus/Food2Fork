bf9a62cdee8448d841287aeca2b7b0f0a90769ab
Ulrik Utheim Sverdrup <ulrik.utheim.sverdrup@gmail.com>
2018-06-15 15:06:56 +0200

    Adds codable struct for the different data types

--- Food2Fork/Recipe.swift
+++ Food2Fork/Recipe.swift
@@ -8,6 +8,35 @@
 
 import Foundation
 
+struct ListRecipe2: Codable { // TODO (UUS): Remove `2`
+    let count: Int
+    let recipes: [Recipe]
+}
+
+struct Recipe: Codable {
+    let publisher: String
+    let food2forkUrl: URL
+    let ingredients: [String]?
+    let sourceUrl: String
+    let recipeId: String
+    let imageUrl: URL
+    let socialRank: Double
+    let publisherUrl: URL
+    let title: String
+    
+    enum CodingKeys: String, CodingKey {
+        case publisher
+        case food2forkUrl = "f2f_url"
+        case ingredients
+        case sourceUrl = "source_url"
+        case recipeId = "recipe_id"
+        case imageUrl = "image_url"
+        case socialRank = "social_rank"
+        case publisherUrl = "publisher_url"
+        case title
+    }
+}
+
 struct ListRecipe {
     let recipeId: String
     let title: String
